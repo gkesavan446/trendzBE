31e5611b2b974952a11e6bfe0b1c539a20331c2e
gkesavan446 <gkesavan446@gmail.com>
2026-08-11 13:32:52 +0530

    message with readability

--- controller/auth.controller.js
+++ controller/auth.controller.js
@@ -125,7 +125,7 @@ const forgotPassword = async (req, res) => {
 
         res.status(200).json({ message: "OTP sent to your email." })
     } catch (error) {
-        res.status(500).json({ message: "Internal Server Error", Error: error.message });
+        res.status(500).json({ message: "The email service may be unavailable on the deployed server", Error: error.message });
     }
 }
 
